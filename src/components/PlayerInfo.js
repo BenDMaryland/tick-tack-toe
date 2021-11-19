f9cc8856af1d267a90b9c2cafbb77bbc16ef9f01
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-19 11:00:07 -0500

    divcenter

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -5,12 +5,15 @@ import ParticlesBG from '../ParticlesBG';
 function PlayerInfo({player:{id,player_name,player_wins,player_losses,player_avatar}}) {
 
     return (
-        <div id="playerinfo"  style={{"display":"inline-block"}} style={{"border":"solid"}}              >
-         <img style={{"display":"inline-block"}}  style={{"width": "150px"}} src={player_avatar}></img>   
-         <h4>{player_name}</h4>
-         <p>Loses: {player_losses}</p>
-         <br />
-         <p>wins: {player_wins}</p>
+        <div       >
+        <div id="playerisnfo"   style={{"border":"solid"}}              >
+        <h4   style={{"textAlign":"center"}} >{player_name}  </h4>
+         <img  id ="playerinfoimage" style={{"width": "50%"}} style={{"marginLeft":"auto"}}style={{"marginRight":"auto"}}   style={{"width": "150px"}} src={player_avatar}></img>   
+        
+         <p style={{"textAlign":"center"}} > Wins: {player_wins} </p>
+         <p style={{"textAlign":"center"}} > Loses: {player_losses} </p>
+        </div>
+        
         </div>
         
     )
