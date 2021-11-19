6ed9ac35a1aaea63a46f5a5a134d55b3f0ebb9e3
Joshua Andes <andesjoshua@comcast.net>
2021-11-19 10:57:10 -0500

    more css changes

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -5,12 +5,11 @@ import ParticlesBG from '../ParticlesBG';
 function PlayerInfo({player:{id,player_name,player_wins,player_losses,player_avatar}}) {
 
     return (
-        <div id="playerinfo"  style={{"display":"inline-block"}} style={{"border":"solid"}}              >
+        <div className="winLossDisplay">
          <img style={{"display":"inline-block"}}  style={{"width": "150px"}} src={player_avatar}></img>   
-         <h4>{player_name}</h4>
-         <p>Loses: {player_losses}</p>
-         <br />
-         <p>wins: {player_wins}</p>
+         <h4></h4>
+         <p> {`${player_name}: `} {`Losses: ${player_losses}`} {`Wins:   ${player_wins}`} </p>
+         <p></p>
         </div>
         
     )
